2fc0f309d937b22615656c3ccabaf968bdccbd96
Jonathan Salazar <jonsalaz23@unm.edu>
2022-04-28 10:25:43 -0600

    Server initialization and client connection.

--- src/AuctionHouse/AuctionHouseApplication.java
+++ src/AuctionHouse/AuctionHouseApplication.java
@@ -1,4 +1,21 @@
 package AuctionHouse;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class AuctionHouseApplication {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(1013);
+            while (true) {
+                System.out.println("Waiting for a connection");
+                Socket socket = server.accept();
+                System.out.println("Client accepted!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
