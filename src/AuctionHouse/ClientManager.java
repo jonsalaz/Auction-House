ea90666646af54f4079079b121c864d3441002f2
Jonathan Salazar <jonsalaz23@unm.edu>
2022-04-28 10:55:24 -0600

    Receive requests from client until "quit" is received

--- src/AuctionHouse/ClientManager.java
+++ src/AuctionHouse/ClientManager.java
@@ -1,6 +1,12 @@
 package AuctionHouse;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.util.Locale;
 
 public class ClientManager implements Runnable {
     Socket client;
@@ -12,5 +18,16 @@ public class ClientManager implements Runnable {
     @Override
     public void run() {
         //TODO client login and request information.
+        try {
+            DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
+            DataOutputStream out = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+            String request = "";
+            while(!request.toLowerCase(Locale.ROOT).equals("quit")) {
+                    request = in.readUTF();
+                    System.out.println("The following request has been made: " + request);
+            }
+
+            client.close();
+        } catch (Exception e) {}
     }
 }
