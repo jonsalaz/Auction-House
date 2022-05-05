73158a8a911f8942c9aa4c4e2cae592f0da2cd9c
Jonathan Salazar <jonsalaz23@unm.edu>
2022-05-05 12:51:58 -0600

    Auction initialization

--- src/AuctionHouse/AHManager.java
+++ src/AuctionHouse/AHManager.java
@@ -1,4 +1,37 @@
 package AuctionHouse;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class AHManager {
+    ArrayList<Auction> auctions;
+
+    public AHManager() {
+        this.auctions = initializeAuctions();
+    }
+
+    private ArrayList<Auction> initializeAuctions() {
+        ArrayList<Auction> initialAuctions = new ArrayList<>();
+        InputStream in = getClass().getResourceAsStream("/items");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        int itemCount = 0;
+        String[] splitLine = null;
+        String line = null;
+        try {
+            line = reader.readLine();
+            while(line != null && itemCount < 3) {
+                splitLine = line.split(" ");
+                Auction auction = new Auction(Integer.parseInt(splitLine[0]),
+                                                splitLine[1],
+                                                Integer.parseInt(splitLine[2]));
+                auctions.add(auction);
+                itemCount++;
+            }
+        } catch (IOException e) {}
+        return initialAuctions;
+    }
 }
---
+++ src/AuctionHouse/Auction.java
@@ -0,0 +1,12 @@
+package AuctionHouse;
+
+public class Auction {
+    private int id;
+    private String name;
+    private int value;
+    public Auction(int id, String name, int value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+}
