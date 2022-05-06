0a62874a492c6a0d6014a67dedfda351becac0a8
Jonathan Salazar <jonsalaz23@unm.edu>
2022-05-05 19:41:35 -0600

    Report winning to agent.

--- src/AuctionHouse/Auction.java
+++ src/AuctionHouse/Auction.java
@@ -1,11 +1,17 @@
 package AuctionHouse;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Auction {
     private int id;
     private String name;
     private int value;
     private float currentBid;
     private long startTime;
+    private Socket winner;
+
     public Auction(int id, String name, int value, long startTime) {
         this.id = id;
         this.name = name;
@@ -44,5 +50,17 @@ public class Auction {
      */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Informs user of winning bid with format "win <id>" and finalizes the process.
+     */
+    public void finish() {
+        if(winner != null){
+            try {
+                DataOutputStream out = new DataOutputStream(new BufferedOutputStream(winner.getOutputStream()));
+                out.writeUTF("win " + this.id);
+            } catch (Exception ignored) {}
+        }
     }
 }
